c2dae8bd7cfa7a66a248e9c7ccff1352cbe5dbfa
yamachu <yamachu.dev@gmail.com>
2020-09-10 02:16:49 +0900

    500で死んでるのこれで握りつぶしてリトライでいいでしょ

--- src/platform/azure/DurableFunctionsOrchestrator/index.ts
+++ src/platform/azure/DurableFunctionsOrchestrator/index.ts
@@ -3,16 +3,22 @@ import * as df from "durable-functions";
 
 module.exports = df.orchestrator(function* (context) {
   const currentValue: string = context.bindings.since_id;
-  const result: {
-    next_since_id: string;
-  } = yield context.df.callActivity(
-    "DurableFunctionsMainActivity",
-    currentValue
-  );
-  const next_since_id = result.next_since_id;
-  context.log(
-    `current_since_id: ${currentValue}, next_since_id: ${next_since_id}`
-  );
+  let mutable_next_since_id = currentValue;
+
+  try {
+    const {
+      next_since_id,
+    }: { next_since_id: string } = yield context.df.callActivity(
+      "DurableFunctionsMainActivity",
+      currentValue
+    );
+    context.log(
+      `current_since_id: ${currentValue}, next_since_id: ${next_since_id}`
+    );
+    mutable_next_since_id = next_since_id;
+  } catch (e) {
+    context.log(e);
+  }
 
   const currentDateTime = dayjs(context.df.currentUtcDateTime).toDate();
   const nextCleanupDateTime = dayjs(currentDateTime).add(5, "m").toDate();
@@ -23,7 +29,7 @@ module.exports = df.orchestrator(function* (context) {
   // ref: https://github.com/Azure/azure-functions-durable-js/issues/126
   // とりあえず return する
   yield context.df.continueAsNew({
-    since_id: next_since_id,
+    since_id: mutable_next_since_id,
   });
   return "";
 });
